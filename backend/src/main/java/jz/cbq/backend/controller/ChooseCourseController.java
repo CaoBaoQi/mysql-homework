5fec56da314e6b8c003deb8850e269a552e403de
曹宝琪 <2023cbq@gmail.com>
2023-12-08 17:53:56 +0800

    backend: chooseCourse CRUD

--- backend/src/main/java/jz/cbq/backend/controller/ChooseCourseController.java
+++ backend/src/main/java/jz/cbq/backend/controller/ChooseCourseController.java
@@ -5,63 +5,76 @@ import jz.cbq.backend.entity.ChooseCourse;
 import jz.cbq.backend.service.IChooseCourseService;
 import jz.cbq.backend.service.ICourseService;
 import jz.cbq.backend.vo.Result;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
 /**
- * AdminController
+ * ChooseCourseController
  *
  * @author caobaoqi
  */
 @RestController
 @RequestMapping("/chooseCourse")
 public class ChooseCourseController {
-    @Autowired
+    @Resource
     private IChooseCourseService chooseCourseService;
-    @Autowired
+    @Resource
     private ICourseService courseService;
+
+    /**
+     * 选课
+     *
+     * @param chooseCourse chooseCourse
+     * @return INFO
+     */
     @Transactional
     @PostMapping("/add")
-    public Result add(@RequestBody ChooseCourse chooseCourse){
+    public Result<String> add(@RequestBody ChooseCourse chooseCourse) {
         String courseId = chooseCourse.getCourseId();
         String stuId = chooseCourse.getStuId();
-        String courseName = chooseCourse.getCourseName();
-        String majorName = chooseCourse.getMajorName();
-        String ifDegree = chooseCourse.getIfDegree();
-        String coursePeriod = chooseCourse.getCoursePeriod();
-        String chooseCourseId=stuId+"choose"+courseId;
+        String chooseCourseId = stuId + "choose" + courseId;
         chooseCourse.setChooseCourseId(chooseCourseId);
 
         chooseCourse.setCreateTime(new Date());
         chooseCourse.setState(1);
 
         boolean save = chooseCourseService.save(chooseCourse);
-        if (save){//线程池
+        if (save) {
             courseService.updateStuChooseNum(courseId);
             return Result.success("选课成功");
         }
         return Result.fail("选课失败");
     }
 
+    /**
+     * 通过学生 id 获取可选课程列表
+     *
+     * @param stuId stuId
+     * @return List<ChooseCourse>
+     */
     @GetMapping("/getCanScoreCourse")
-    public Result getCanScoreCourse(String stuId){
-        LambdaQueryWrapper<ChooseCourse> chooseCourseQueryWrapper = new LambdaQueryWrapper<>();
-        chooseCourseQueryWrapper.eq(ChooseCourse::getStuId,stuId);
-        List<ChooseCourse> canScoreCourseList = chooseCourseService.list(chooseCourseQueryWrapper);
+    public Result<List<ChooseCourse>> getCanScoreCourse(String stuId) {
+        List<ChooseCourse> canScoreCourseList = chooseCourseService.list(new LambdaQueryWrapper<ChooseCourse>().eq(ChooseCourse::getStuId, stuId));
         return Result.success(canScoreCourseList);
     }
+
+    /**
+     * 取消所有选课
+     *
+     * @return INFO
+     */
     @Transactional
     @DeleteMapping("/cancelChooseCourse")
-    public Result cancelChooseCourse(){
-        long start=System.currentTimeMillis();
-        int cancelChooseCourse=chooseCourseService.cancelChooseCourse();
-        if (cancelChooseCourse>=1){
+    public Result<String> cancelChooseCourse() {
+        long start = System.currentTimeMillis();
+        int cancelChooseCourse = chooseCourseService.cancelChooseCourse();
+        if (cancelChooseCourse >= 1) {
             courseService.updateChooseCourseNum();
-            long end=System.currentTimeMillis();
+            long end = System.currentTimeMillis();
             return Result.success("一键退课完毕,服务器花费时间" + (end - start) + "ms");
         }
         return Result.fail("一键退课失败");
