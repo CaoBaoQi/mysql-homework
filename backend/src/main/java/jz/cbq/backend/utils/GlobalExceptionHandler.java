9944e64c7f7e41b610623634bb0b14c12b4aad28
曹宝琪 <2023cbq@gmail.com>
2023-12-08 15:53:33 +0800

    backend: exception

--- backend/src/main/java/jz/cbq/backend/utils/GlobalExceptionHandler.java
+++ backend/src/main/java/jz/cbq/backend/utils/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package jz.cbq.backend.utils;
 
 import jz.cbq.backend.vo.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 /**
  * 异常拦截器
@@ -11,11 +14,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author caobaoqi
  */
 @ControllerAdvice
-public class AllExceptionHandler {
+@Slf4j
+public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public Result doException(Exception e) {
-        System.out.println(e.getMessage());
+    public Result<String> doException(Exception exception) {
+        if (log.isDebugEnabled()) {
+            log.warn("GlobalExceptionHandler " + exception.getMessage());
+            log.warn("=========ExceptionINFO-Start===============");
+            log.error("ERROR DETAILS " ,exception);
+            log.warn("=========ExceptionINFO-END===============");
+
+        }
         return Result.fail("服务器出错 | 或存在关联信息请联系 1203952894@qq.com");
     }
 }
