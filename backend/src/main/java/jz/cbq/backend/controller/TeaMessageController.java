30a569a4483641f11c2cf8d1233cdf5752de727a
曹宝琪 <2023cbq@gmail.com>
2023-12-08 17:59:55 +0800

    backend: teaMessage CRUD

--- backend/src/main/java/jz/cbq/backend/controller/TeaMessageController.java
+++ backend/src/main/java/jz/cbq/backend/controller/TeaMessageController.java
@@ -5,47 +5,64 @@ import jz.cbq.backend.entity.TeaMessage;
 import jz.cbq.backend.entity.TeaMsgMap;
 import jz.cbq.backend.service.ITeaMessageService;
 import jz.cbq.backend.vo.Result;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 /**
- * AdminController
+ * TeaMessageController
  *
  * @author caobaoqi
  */
 @RestController
 @RequestMapping("teaMessage")
 public class TeaMessageController {
-    @Autowired
+    @Resource
     private ITeaMessageService teaMessageService;
 
+    /**
+     * 通过老师 id 和学生姓名 获取消息列表
+     *
+     * @param teaId   teaId
+     * @param stuName stuName
+     * @return List<TeaMsgMap>
+     */
     @GetMapping("/getMsgsByTeaId")
-    public Result getMsgsByTeaId(String teaId, @RequestParam(value = "stuName",required = false) String stuName){
-        List<TeaMsgMap> allTeaMsg =teaMessageService.getAllTeaMsg(teaId,stuName);
+    public Result<List<TeaMsgMap>> getMsgsByTeaId(String teaId, @RequestParam(value = "stuName", required = false) String stuName) {
+        List<TeaMsgMap> allTeaMsg = teaMessageService.getAllTeaMsg(teaId, stuName);
         return Result.success(allTeaMsg);
     }
-    @PostMapping("/addMsg")//新增消息
-    public Result addMsg(@RequestBody TeaMessage teaMessage){
-        teaMessage.setMsgId(teaMessage.getTeaId()+"msg"+ teaMessage.getStuId()+ UUID.randomUUID());
+
+    /**
+     * 添加消息
+     *
+     * @param teaMessage teaMessage
+     * @return INFO
+     */
+    @PostMapping("/addMsg")
+    public Result<String> addMsg(@RequestBody TeaMessage teaMessage) {
+        teaMessage.setMsgId(teaMessage.getTeaId() + "msg" + teaMessage.getStuId() + UUID.randomUUID());
         teaMessage.setMsgTime(new Date());
         boolean save = teaMessageService.save(teaMessage);
-        if (save){
+        if (save) {
             return Result.success("发消息成功");
         }
         return Result.fail("发消息失败");
     }
+
+    /**
+     * 根据 id 删除
+     *
+     * @param msgId msgId
+     * @return INFO
+     */
     @DeleteMapping("/delById")
-    public Result delById(String msgId){
-        LambdaQueryWrapper<TeaMessage> msgQueryWrapper = new LambdaQueryWrapper<>();
-        msgQueryWrapper.eq(TeaMessage::getMsgId,msgId);
-        boolean remove = teaMessageService.remove(msgQueryWrapper);
-        if (remove){
-            return Result.success("删除消息成功");
-        }
-        return Result.fail("删除消息失败");
+    public Result<String> delById(String msgId) {
+        boolean remove = teaMessageService.remove(new LambdaQueryWrapper<TeaMessage>().eq(TeaMessage::getMsgId, msgId));
+
+        return remove ? Result.success("删除消息成功") : Result.fail("删除消息失败");
     }
 }
