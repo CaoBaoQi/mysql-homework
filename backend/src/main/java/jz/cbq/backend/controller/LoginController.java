3df9846fb0a6efe3e291b518743818624bf90612
曹宝琪 <2023cbq@gmail.com>
2023-12-08 18:02:34 +0800

    backend: login CRUD

--- backend/src/main/java/jz/cbq/backend/controller/LoginController.java
+++ backend/src/main/java/jz/cbq/backend/controller/LoginController.java
@@ -11,75 +11,87 @@ import jz.cbq.backend.service.ILoginService;
 import jz.cbq.backend.service.IStudentService;
 import jz.cbq.backend.service.ITeacherService;
 import jz.cbq.backend.utils.JWTUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
- * AdminController
+ * LoginController
  *
  * @author caobaoqi
  */
 @RestController
 public class LoginController {
-    @Autowired
+    @Resource
     private IAdminService adminService;
-    @Autowired
+    @Resource
     private IStudentService studentService;
-    @Autowired
+    @Resource
     private ITeacherService teacherService;
-    @Autowired
-    private RedisTemplate<String,String> redisTemplate;
-    @Autowired
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+    @Resource
     private PasswordEncoder passwordEncoder;
-    @Autowired
+    @Resource
     private ILoginService loginService;
+
+    /**
+     * 登录
+     *
+     * @param id       id
+     * @param password password
+     * @param state    state
+     * @return HashMap<String, Object>
+     */
     @PostMapping("/login")
-    public Result login(String id,String password,String state){
+    public Result<HashMap<String, Object>> login(String id, String password, String state) {
         HashMap<String, Object> data = new HashMap<>();
         String token = JWTUtils.createToken(id);
-        data.put("token",token);
-        data.put("state",state);
-        if ("管理员".equals(state)){
-            LambdaQueryWrapper<Admin> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(Admin::getAdminId, id);
-            Admin adminDatabase = adminService.getOne(queryWrapper);
-            if (null != adminDatabase && passwordEncoder.matches(password, adminDatabase.getAdminPwd())) {
-                redisTemplate.opsForValue().set(token, JSON.toJSONString(adminDatabase), 1, TimeUnit.DAYS);//1天
-                data.put("user",adminDatabase);
-                return Result.success(data, state+"登录成功");
+        data.put("token", token);
+        data.put("state", state);
+        switch (state) {
+            case "管理员":
+                Admin adminDatabase = adminService.getOne(new LambdaQueryWrapper<Admin>().eq(Admin::getAdminId, id));
+                if (null != adminDatabase && passwordEncoder.matches(password, adminDatabase.getAdminPwd())) {
+                    redisTemplate.opsForValue().set(token, JSON.toJSONString(adminDatabase), 1, TimeUnit.DAYS);
+                    data.put("user", adminDatabase);
+                    return Result.success(data, state + "登录成功");
+                }
+                return Result.fail(state + "不存在或密码错误");
+            case "学生": {
+                Student stuDatabase = studentService.getOne(new LambdaQueryWrapper<Student>().eq(Student::getStuId, id));
+                if (null != stuDatabase && passwordEncoder.matches(password, stuDatabase.getStuPwd())) {
+                    redisTemplate.opsForValue().set(token, JSON.toJSONString(stuDatabase), 1, TimeUnit.DAYS);
+                    data.put("user", stuDatabase);
+                    return Result.success(data, state + "登录成功");
+                }
+                return Result.fail(state + "不存在或密码错误");
             }
-            return Result.fail(state+"不存在或密码错误");
-        }
-        else if ("学生".equals(state)){
-            LambdaQueryWrapper<Student> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(Student::getStuId, id);
-            Student stuDatabase = studentService.getOne(queryWrapper);
-            if (null != stuDatabase && passwordEncoder.matches(password, stuDatabase.getStuPwd())){
-                redisTemplate.opsForValue().set(token, JSON.toJSONString(stuDatabase), 1, TimeUnit.DAYS);//1天
-                data.put("user",stuDatabase);
-                return Result.success(data, state+"登录成功");
+            case "班主任": {
+                Teacher teaDatabase = teacherService.getOne(new LambdaQueryWrapper<Teacher>().eq(Teacher::getTeaId, id));
+                if (null != teaDatabase && passwordEncoder.matches(password, teaDatabase.getTeaPwd())) {
+                    redisTemplate.opsForValue().set(token, JSON.toJSONString(teaDatabase), 1, TimeUnit.DAYS);
+                    data.put("user", teaDatabase);
+                    return Result.success(data, state + "登录成功");
+                }
+                return Result.fail(state + "不存在或密码错误");
             }
-            return Result.fail(state+"不存在或密码错误");
-        }
-        else if ("班主任".equals(state)){
-            LambdaQueryWrapper<Teacher> queryWrapper = new LambdaQueryWrapper<>();
-            queryWrapper.eq(Teacher::getTeaId, id);
-            Teacher teaDatabase = teacherService.getOne(queryWrapper);
-            if (null != teaDatabase && passwordEncoder.matches(password, teaDatabase.getTeaPwd())){
-                redisTemplate.opsForValue().set(token, JSON.toJSONString(teaDatabase), 1, TimeUnit.DAYS);//1天
-                data.put("user",teaDatabase);
-                return Result.success(data, state+"登录成功");
-            }
-            return Result.fail(state+"不存在或密码错误");
         }
         return Result.fail("没有选择身份");
     }
+
+    /**
+     * 退出登录
+     *
+     * @param token token
+     * @return INO
+     */
     @GetMapping("/logout")
-    public Result adminLogout(@RequestHeader("Authorization")String token){
+    public Result<String> adminLogout(@RequestHeader("Authorization") String token) {
         loginService.logout(token);
         return Result.success("退出登录成功");
     }
