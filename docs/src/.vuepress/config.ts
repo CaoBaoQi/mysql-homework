774f5428ba9b977f495326989da44eeda72b8a66
曹宝琪 <2023cbq@gmail.com>
2023-12-09 23:29:02 +0800

    pages

--- docs/src/.vuepress/config.ts
+++ docs/src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/mysql-homework/",
 
   lang: "zh-CN",
   title: "MySQL-Study",
