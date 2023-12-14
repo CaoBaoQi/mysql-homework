d40e4b1e8a0597c62ad22181a02fdd7a6d5a2478
曹宝琪 <2023cbq@gmail.com>
2023-12-15 00:37:20 +0800

    book-demo: init commit

---
+++ book-demo-backend/src/main/java/jz/cbq/bookdemobackend/BookDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package jz.cbq.bookdemobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookDemoBackendApplication.class, args);
+    }
+
+}
